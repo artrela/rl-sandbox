ef8850fcdb70c477d6876a211352902f09966085
artrela <trelaalec@gmail.com>
2023-12-26 11:13:27 -0500

    cuda enabled

--- dqn.py
+++ dqn.py
@@ -8,27 +8,29 @@ import torch.nn as nn
 
 class DQN(nn.Module): 
     
-    def __init__(self, input, max_exp, action_space):
+    def __init__(self, input, max_exp, action_space, device):
         
         self.input_space = input
+        self.device = device
         
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=4, stride=2, padding=4)
-        self.pool = nn.MaxPool2d(kernel_size=4, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=4, out_channels=2, kernel_size=8, stride=2, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=8, stride=4, device=device)
+        self.relu_conv1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, device=device)
+        self.relu_conv2 = nn.ReLU()
         
-        self.fc1 = nn.Linear(3626, 2460)
+        self.fc1 = nn.Linear(13824, 256, device=device)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(2460, 1800)
-        self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(1800, 700)
-        self.relu3 = nn.ReLU()
-        self.fc4 = nn.Linear(700, 120)
-        self.relu4 = nn.ReLU()
-        self.fc5 = nn.Linear(120, action_space)
+        # self.fc2 = nn.Linear(1850, 1100)
+        # self.relu2 = nn.ReLU()
+        # self.fc3 = nn.Linear(1100, 500)
+        # self.relu3 = nn.ReLU()
+        # self.fc4 = nn.Linear(500, 256)
+        # self.relu4 = nn.ReLU()
+        self.fc5 = nn.Linear(256, action_space, device=device)
         
         self.loss = nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.0001)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
         
         self.experiences = deque(maxlen=max_exp)
         
@@ -42,20 +44,21 @@ class DQN(nn.Module):
     def forward(self, x):
         
         out = self.conv1(x) 
-        out = self.pool(out)       
+        out = self.relu_conv1(out)
         out = self.conv2(out)
+        out = self.relu_conv2(out)
         
         out = self.fc1(torch.flatten(out))
         out = self.relu1(out)
         
-        out = self.fc2(out)
-        out = self.relu2(out)
+        # out = self.fc2(out)
+        # out = self.relu2(out)
         
-        out = self.fc3(out)
-        out = self.relu3(out)
+        # out = self.fc3(out)
+        # out = self.relu3(out)
         
-        out = self.fc4(out)
-        out = self.relu4(out)
+        # out = self.fc4(out)
+        # out = self.relu4(out)
         
         return self.fc5(out)
     
@@ -79,10 +82,10 @@ class DQN(nn.Module):
                 yj = r
             else:
                 with torch.no_grad():
-                    yj = r + gamma * torch.max((self.forward(self.convert(next_s))))
+                    yj = r + gamma * torch.max((self.forward(self.convert(next_s).to(self.device))))
                 
             # Perform a gradient descent step on (yj − Q(φj , aj ; θ))^2 according to equation 3
-            Q_sa = self.forward(self.convert(s))
+            Q_sa = self.forward(self.convert(s).to(self.device))
             
             targets[i] = yj
             current[i] = Q_sa[a]
--- main.py
+++ main.py
@@ -10,31 +10,43 @@ import torch
 def main(args):
     
     if args.render:
-        env = gym.make("ALE/DonkeyKong-v5", render_mode="human", frameskip=10, obs_type="grayscale")
+        env = gym.make("ALE/DonkeyKong-v5", render_mode="human", frameskip=4, obs_type="grayscale")
     else:
-        env = gym.make("ALE/DonkeyKong-v5", frameskip=10, obs_type="grayscale")
+        env = gym.make("ALE/Breakout-v5", frameskip=4, obs_type="grayscale")
         
     observation, info = env.reset()
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using Device: {device}")
-    model = DQN(observation.shape, args.max_exp, env.action_space.n)
+    
+    model = DQN(
+        observation.shape, 
+        args.max_exp, 
+        env.action_space.n, 
+        device
+        )
+    
     model.to(device)
     Experience = namedtuple("Experience", ["state", "reward", "action", "next_state", "terminal"])
     
     # plt.imshow(observation)
     # plt.show()
     
-    fig, ax = plt.subplots(2, 2)
+    fig, ax = plt.subplots(2, 3, figsize=(20, 10))
     plt.tight_layout()
     
     end_episode_loss = []
+    end_episode_avgQ = []
     actions_taken = [0 for i in range(env.action_space.n)]
     returns = []
+    episode_len = []
+    epsilon_decay = []
+    total_steps = 0
+    end_decay = 1000000
+    decay_rate = lambda x: ((1 - args.eps) / end_decay) * x
         
     for e in range(args.episodes):
         
-        avg_return = 0
         in_epsisode_avg_returns = []
         in_epsisode_avg_Q = []
         in_episode_loss = []
@@ -42,16 +54,21 @@ def main(args):
         i = 0
         
         terminated = truncated = False
-        while not terminated or truncated:
+        while not (terminated or truncated):
             
+            if total_steps < end_decay: 
+                eps = 1 - decay_rate(total_steps)
+            else:
+                eps = args.eps
+                        
             ''' 
             With probability eps select a random action at
             otherwise select at = maxa Q*(φ(st), a; θ)
             '''
             P = random.random()
-            if P < 1 - args.eps:
+            if P < 1 - eps:
                 with torch.no_grad(): # guess we need this
-                    actions = model(model.convert(observation)) # use the network to find the set of actions 
+                    actions = model(model.convert(observation).to(device)) # use the network to find the set of actions 
                     action = torch.argmax(actions)
             else:
                 action = env.action_space.sample()  # agent policy that uses the observation and info
@@ -69,7 +86,7 @@ def main(args):
                 continue
             
             # accure enough experiences first
-            if len(model.experiences) < model.experiences.maxlen:
+            if len(model.experiences) < args.batch_size:
                 continue
             
             ''' 
@@ -91,22 +108,30 @@ def main(args):
             in_epsisode_avg_returns.append(torch.mean(rewards).detach().numpy())
             in_epsisode_avg_Q.append(torch.mean(Q_sa).detach().numpy())
             
+            total_steps += 1
             i += 1
-            if i % 10 == 0:
+            if i % 15 == 0:
+                
                 in_episode_loss.append((output).detach().numpy())
                 in_episode_steps.append(i)
-                if i % 20 == 0:
-                    print(f"Loss at Episode {e+1} Step {i}: {round(float(in_episode_loss[-1]), 4)}")
-                ax[0, 0].plot(in_episode_steps, in_episode_loss)
-                ax[0, 0].set_title("In Episode Loss")
+                epsilon_decay.append(eps)
+                
+                # if i % 50 == 0:
+                #     print(f"Loss at Episode {e+1} Step {i}: {round(float(in_episode_loss[-1]), 4)}")
+                    
+                ax[0, 0].plot(range(15, (len(epsilon_decay)+1) * 15, 15), epsilon_decay)
+                ax[0, 0].set_title("Epsilon")
                 ax[0, 0].grid()
-                ax[0, 1].plot(range(1, len(in_epsisode_avg_Q)+1), in_epsisode_avg_Q)
-                ax[0, 1].set_title("Average Q")
-                ax[0, 1].grid()
-                ax[1, 1].plot(range(1, len(in_epsisode_avg_returns) + 1), in_epsisode_avg_returns)
-                ax[1, 1].set_title("Returns")
-                ax[1, 1].grid()
-                fig.savefig(args.out)
+                # ax[0, 1].plot(range(1, len(in_epsisode_avg_Q)+1), in_epsisode_avg_Q)
+                # ax[0, 1].set_title("Average Q within Episode")
+                # ax[0, 1].grid()
+                # ax[1, 2].bar(range(len(actions_taken)), actions_taken)
+                # ax[1, 2].set_xticks(range(len(actions_taken)))
+                # ax[1, 2].set_title("Actions")
+                try:
+                    fig.savefig(args.out)
+                except PermissionError:
+                    pass
             
             # backprop
             model.optimizer.zero_grad()
@@ -117,20 +142,41 @@ def main(args):
         # reset before next episode
         observation, info = env.reset()
         returns.append(sum(in_epsisode_avg_returns) / i)
+        end_episode_avgQ.append(sum(in_epsisode_avg_Q) / i)
         end_episode_loss.append(sum(in_episode_loss) / i)
+        episode_len.append(i)
         
         print("===========================")
         print(f"End of Episode {e+1}:") 
         print(f"Average Return | {round(returns[-1], 4)}") 
         print(f"Average Loss   | {round(end_episode_loss[-1], 4)}") 
+        print(f"Episode Length | {i}") 
+        print(f"Epsilon        | {round(eps, 4)}") 
+        print(f"Total Steps    | {total_steps}") 
         print("===========================")
         
+        ax[0, 2].plot(range(1, len(end_episode_avgQ)+1), end_episode_avgQ)
+        ax[0, 2].set_title("Average Q Returns")
+        ax[0, 2].grid()
+        ax[0, 1].bar(range(len(actions_taken)), actions_taken)
+        ax[0, 1].set_xticks(range(len(actions_taken)))
+        ax[0, 1].set_title("Actions")
         ax[1, 0].plot(range(1, len(end_episode_loss) + 1), end_episode_loss)
         ax[1, 0].set_title("End of Episode Loss")
         ax[1, 0].grid()
-        fig.savefig(args.out)
+        ax[1, 1].plot(range(1, len(returns) + 1), returns)
+        ax[1, 1].set_title("Returns")
+        ax[1, 1].grid()
+        ax[1, 2].plot(range(1, len(episode_len) + 1), episode_len)
+        ax[1, 2].grid()
+        ax[1, 2].set_title("Epsisode Length")
+        try:
+            fig.savefig(args.out)
+        except PermissionError:
+            pass
     
     env.close()
+    torch.save(model, "dqn.pth")
 
 if __name__ == "__main__":
     
@@ -138,10 +184,10 @@ if __name__ == "__main__":
     
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--out", default="plot.png", type=str)
-    parser.add_argument("--episodes", default=100, type=int)
+    parser.add_argument("--episodes", default=1000, type=int)
     parser.add_argument("--batch_size", default=32, type=int)
-    parser.add_argument("--max_exp", default=256, type=int)
-    parser.add_argument("--eps", default=0.15, type=float)
+    parser.add_argument("--max_exp", default=1024, type=int)
+    parser.add_argument("--eps", default=0.1, type=float)
     parser.add_argument("--gamma", default=0.9, type=float)
     args = parser.parse_args()
     
